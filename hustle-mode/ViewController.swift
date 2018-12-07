7dd2e28a0962ea0108c3292f6567a0c6d393f612
Kotamoss <storyofko@gmail.com>
2018-12-07 01:09:48 -0500

    first commit

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by R$KY on 12/7/18.
+//  Copyright © 2018 Dakotah Moss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
