018641a35541f5a0880afbc2741e966f31fbd591
Kotamoss <storyofko@gmail.com>
2018-12-10 17:35:04 -0500

    Added IBOutlets for all labels, buttons, and images on the view controller.

--- hustle-mode/ViewController.swift
+++ hustle-mode/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var darkBlueBG: UIImageView!
+    @IBOutlet weak var powerBtn: UIButton!
+    @IBOutlet weak var cloudHolder: UIView!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var hustleLbl: UILabel!
+    @IBOutlet weak var onLbl: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
